72479ba5b89224e3b5d4c01cc2c56c06870b7b55
cglotr <amir.ariffin.920404@gmail.com>
2019-11-24 12:57:17 +0800

    test sura response

--- server/src/surahandler_test.go
+++ server/src/surahandler_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,6 +12,33 @@ func TestSuraHandler(t *testing.T) {
 	testSuraQuery(t, "suraNumber=1", http.StatusBadRequest)
 	testSuraQuery(t, "verseNumber=1", http.StatusBadRequest)
 	testSuraQuery(t, "suraNumber=1&verseNumber=1", http.StatusOK)
+	testSuraResponse(t)
+}
+
+func testSuraResponse(t *testing.T) {
+	r := httptest.NewRequest("GET", "http://example.com/sura?suraNumber=1&verseNumber=1", nil)
+	q := &stub{}
+	w := httptest.NewRecorder()
+	suraHandler(q)(w, r)
+
+	var gots []verse
+	var wants []verse
+
+	json.NewDecoder(w.Result().Body).Decode(&gots)
+	wants, err := q.getVerses(1, 1, 1)
+	if err != nil {
+		t.FailNow()
+	}
+	if len(gots) < 1 {
+		t.FailNow()
+	}
+	for i := 0; i < len(wants); i++ {
+		got := gots[i]
+		want := wants[i]
+		if got != want {
+			t.Errorf("got %#v; want %#v", got, want)
+		}
+	}
 }
 
 func testSuraQuery(t *testing.T, query string, statusCode int) {
