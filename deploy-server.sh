832818fbf6a775279b54602395e9bca4bbd221f2
cglotr <amir.ariffin.920404@gmail.com>
2019-11-24 11:32:17 +0800

    change to cglotr image repo

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,5 +1,5 @@
-docker build -t arikama/koran-server -t arikama/koran-server:$(git rev-parse HEAD) ./server
-docker push arikama/koran-server:latest
-docker push arikama/koran-server:$(git rev-parse HEAD)
+docker build -t cglotr/koran-server -t cglotr/koran-server:$(git rev-parse HEAD) ./server
+docker push cglotr/koran-server:latest
+docker push cglotr/koran-server:$(git rev-parse HEAD)
 kubectl apply -f kubernetes/deployment-server.yaml
-kubectl set image deployments/deployment-server server=arikama/koran-server:$(git rev-parse HEAD)
+kubectl set image deployments/deployment-server server=cglotr/koran-server:$(git rev-parse HEAD)
