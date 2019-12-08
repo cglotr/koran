d8141b36feea938873fd3ce2849f51e5e2e2fff4
cglotr <amir.ariffin.920404@gmail.com>
2019-12-08 18:41:49 +0800

    install babel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,20 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
