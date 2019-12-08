338e22c485ed938b1bd9fde7a4ded2596eb7457f
cglotr <amir.ariffin.920404@gmail.com>
2019-12-08 18:01:49 +0800

    force use latest image

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,3 +1,5 @@
-docker build -t cglotr/koran-server ./server
+docker build -t cglotr/koran-server -t cglotr/koran-server:$(git rev-parse HEAD) ./server
 docker push cglotr/koran-server:latest
+docker push cglotr/koran-server:$(git rev-parse HEAD)
 kubectl apply -f kubernetes/deployment-server.yml
+kubectl set image deployments/deployment-server server=cglotr/koran-server:$(git rev-parse HEAD)
