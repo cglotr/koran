078ed68c17907690218c19b55746bbe2359aaba6
cglotr <amir.ariffin.920404@gmail.com>
2019-12-14 11:26:19 +0800

    remove unused funcs

--- server/src/main_test.go
+++ server/src/main_test.go
@@ -8,15 +8,3 @@ func TestMain(m *testing.M) {
 	// main()
 	// os.Exit(m.Run())
 }
-
-func assertInt(t *testing.T, got, want int) {
-	if got != want {
-		t.Errorf("got %#v; want %#v", got, want)
-	}
-}
-
-func assertString(t *testing.T, got, want string) {
-	if got != want {
-		t.Errorf("got %#v; want %#v", got, want)
-	}
-}
