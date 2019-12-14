55bba8e3d78b3029e9600eb58394e436eb2b41e6
cglotr <amir.ariffin.920404@gmail.com>
2019-12-14 11:47:56 +0800

    base handler

--- server/src/handlers/gethandler.go
+++ server/src/handlers/gethandler.go
@@ -1,19 +1,16 @@
 package handlers
 
 import (
-	"log"
 	"net/http"
 )
 
 // GetHandler .
 func GetHandler(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.URL.RawPath)
-		w.Header().Add("Content-Type", "application/json;charset=utf-8")
+		BaseHandler(h)(w, r)
 		if r.Method != http.MethodGet {
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
-		h(w, r)
 	}
 }
