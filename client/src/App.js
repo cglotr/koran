014ea34ef7cf39f077421d5d145ff1d5e1aa88ff
cglotr <amir.ariffin.920404@gmail.com>
2019-12-15 12:10:52 +0800

    main container

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import 'regenerator-runtime/runtime'
+import styled from 'styled-components'
+import Container from '@material-ui/core/Container'
 import { Drawer } from '@app/components'
+import { dimensions } from '@app/constants'
 import { Quran, Sura } from './pages'
+
+const Page = styled.div`
+  padding-left: ${dimensions.LENGTH_200}px;
+`
 
 export default class App extends Component {
   render () {
     return (
-      <>
-        <BrowserRouter>
-          <Drawer />
-          <Switch>
-            <Route exact path='/'>
-              <Quran />
-            </Route>
-            <Route path='/sura/:number'>
-              <Sura />
-            </Route>
-          </Switch>
-        </BrowserRouter>
-      </>
+      <BrowserRouter>
+        <Drawer />
+        <Switch>
+          <Page>
+            <Container maxWidth='md'>
+              <Route exact path='/'>
+                <Quran />
+              </Route>
+              <Route path='/sura/:number'>
+                <Sura />
+              </Route>
+            </Container>
+          </Page>
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
