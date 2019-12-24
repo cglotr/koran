6251496a6f51695dd7ad6c5643653264a5459460
cglotr <amir.ariffin.920404@gmail.com>
2019-12-25 01:27:57 +0800

    clean file before appending

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-echo "CLIENT_FIREBASE_API_KEY=$CLIENT_FIREBASE_API_KEY" >> client/.env
+echo "CLIENT_FIREBASE_API_KEY=$CLIENT_FIREBASE_API_KEY" > client/.env
 
 docker build -t cglotr/koran-client -t cglotr/koran-client:$(git rev-parse HEAD) ./client
 docker build -t cglotr/koran-server -t cglotr/koran-server:$(git rev-parse HEAD) ./server
