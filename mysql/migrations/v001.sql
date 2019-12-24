d518b58bdf488347942bcb948fd9efbcd468f558
cglotr <amir.ariffin.920404@gmail.com>
2019-12-24 21:54:04 +0800

    add user table

--- mysql/migrations/v001.sql
+++ mysql/migrations/v001.sql
@@ -1,9 +1,9 @@
 START TRANSACTION;
 CREATE TABLE quran (
-  id           INT           NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  sura_id      INT           NOT NULL,
-  verse_id     INT           NOT NULL,
-  ayah         VARCHAR(2048) NOT NULL,
+  id       INT           NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  sura_id  INT           NOT NULL,
+  verse_id INT           NOT NULL,
+  ayah     VARCHAR(2048) NOT NULL,
   CONSTRAINT UNIQUE KEY (sura_id, verse_id)
 );
 INSERT INTO migration (version_name) VALUES ("v001");
