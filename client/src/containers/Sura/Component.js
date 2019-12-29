c56a670ed72a072e4a7e76a3a02cd76833ab4d35
cglotr <amir.ariffin.920404@gmail.com>
2019-12-30 00:21:00 +0800

    fix lint recursive dir regex

--- client/src/containers/Sura/Component.js
+++ client/src/containers/Sura/Component.js
@@ -1,7 +1,6 @@
 import _ from 'lodash'
 import PropTypes from 'prop-types'
 import React from 'react'
-import styled from 'styled-components'
 import { Column, Footer, Verse } from '@app/components'
 import { Typography } from '@material-ui/core'
 import {
